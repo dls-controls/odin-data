386c032ae0e9fa6583d11334c7883964a45ba9f1
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-01-24 15:07:03 +0000

    Update coverage to report 0% for files that aren't executed

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Make a dummy .gcda for any .gcno files without one. This makes coverage report 0% for files that aren't executed.
+find . -name "*.gcno" -exec sh -c 'touch -a "${1%.gcno}.gcda"' _ {} \;
+
 # Find all .gcda files and run gcov on them with the output as the current directory
 echo "Running gcov on all .gcda files..."
 find . -name "*.gcda" -exec gcov -p -o {} ./ \;
-
 
 # Run remote codecov script to upload - gcov is disabled as we already have the reports
 echo "Uploading coverage data..."
